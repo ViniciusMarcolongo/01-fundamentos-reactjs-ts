3b5dff2c4160d5251ef0f15770abbd7b93a89b8f
Vinicius Marcolongo <141753518+ViniciusMarcolongo@users.noreply.github.com>
2025-04-08 17:16:56 -0300

    Pirmeiro commit
    Troquei o código para TYPESCRIPT

---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import {PencilLine} from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar(){
+    return(
+        <aside className={styles.sidebar}>
+             <img className={styles.cover} 
+             src="https://plus.unsplash.com/premium_photo-1740997621838-faaec5fa62d3?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div className={styles.profile}>
+                <Avatar src="https://s.aficionados.com.br/imagens/coringa-brianbolland.jpg" />
+
+                <strong>Vinicius Marcolongo</strong>
+                <span>FullStack Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
